efe1982e9ccefefc4383f9b1aff7202d30c29901
Nihal <reenasheikh@gmail.com>
2024-09-27 04:48:38 +0530

    New data added in variables

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter your name: "
+read username
+echo "The user name you enterd is: $username"
+echo "confirm if it's correct (yes/no) :"
+read answer
+
+if ["$answer"=="Yes"];then
+      echo "Thank you, $username!"
+else
+      echo"Please enter your correct username. Thank you!"
+
+fi
