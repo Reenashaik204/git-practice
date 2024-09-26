22c88bcb29a2bcd0f2ea9c23e83d049bf471c77c
Nihal <reenasheikh@gmail.com>
2024-09-26 06:24:57 +0530

    madeperson changes

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$Person1="Ramesh"
-$Person2="Suri"
-echo "$Person1: : "Hi $Person2,how are you?  
+Person1="Ramesh"
+Person2="Suri"
+echo "$Person1: :Hi $Person2,how are you?"
 
 echo "$Person2: :Hello $Person1, I am fine, how are you doing?" 
 
-echo "$Person1: : I am doing good $Person2, whats going on?" 
+echo "$Person1: :I am doing good $Person2, whats going on?" 
 
-echo "$Person2: : I started learning shell script" 
+echo "$Person2: :I started learning shell script" 
