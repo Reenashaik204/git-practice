eadccf417e799f32e08131df89ac0d39b34fc4e7
Nihal <reenasheikh@gmail.com>
2024-09-27 04:56:44 +0530

    script added in 05 variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,9 +6,9 @@ echo "The user name you enterd is: $username"
 echo "confirm if it's correct (yes/no) :"
 read answer
 
-if ["$answer"=="Yes"];then
+if [ "$answer" = "Yes" ] || [ "$answer" = "yes" ];then
       echo "Thank you, $username!"
 else
-      echo"Please enter your correct username. Thank you!"
+      echo "Please enter your correct username. Thank you!"
 
 fi
