97add43dfbc204d2fffd09a0f72eff01c4b85f01
Nihal <reenasheikh@gmail.com>
2024-09-26 05:34:37 +0530

    added converstaion script

---
+++ 02-conversations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Ramesh: : "Hi Suresh,how are you?  
+
+Echo "Suresh: :Hello Ramesh, I am fine, how are you doing?" 
+
+Echo "Ramesh: : I am doing good Suresh, whats going on?" 
+
+Echo "Suresh: : I started learning shell script" 
