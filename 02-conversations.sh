7279fad10cef70bd4a262d45042423b1a7ede8a8
Nihal <reenasheikh@gmail.com>
2024-09-26 05:59:53 +0530

    made name changes in convo file

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$Person1=Ramesh
-$Person2=Suri
+$Person1="Ramesh"
+$Person2="Suri"
 echo "$Person1: : "Hi $Person2,how are you?  
 
 echo "$Person2: :Hello $Person1, I am fine, how are you doing?" 
