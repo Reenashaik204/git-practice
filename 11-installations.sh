f22f1ead95aa4345cfd63cf71176f05fdcea831e
Nihal <reenasheikh@gmail.com>
2024-09-27 22:15:46 +0530

    conditions file created

--- 11-installations.sh
+++ 11-installations.sh
@@ -7,4 +7,6 @@ USERID=$( id -u )
     if [ $USERID -ne 0 ]; then
         echo "Please run the script with root privileges"
         exit 1
+    else    
+        echo "Script is running with root privileges"    
     fi
