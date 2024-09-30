8a8f9df9941bd0eb7bba3e3b3d48b5628aac8536
Nihal <reenasheikh@gmail.com>
2024-09-30 20:14:05 +0530

    15th file created

---
+++ 15-loops.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$( id -u )
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]; then
+      echo -e "$R Please run this script with root priviliges $N "
+      exit 1
+    fi
+
+}
+VALIDATE(){
+  if [  $1 -ne 0 ]; then
+     echo -e "$G $2 is failed $N "
+     exit 1
+  else
+     echo -e "$G $2 is success $N "
+
+  fi
+
+}
+
+
+CHECK_ROOT
+
+# sh 15-loops.sh git mysql nginx postfix
+for package in $@ #refers to all args passed to it
+do 
+
+done    
